caa997077c0c61048f2bf87f47d6b1a520a17394
Kyerstin O'Neal <kcrocker1@cnm.edu>
2021-04-18 21:25:52 -0600

    Initial commit

---
+++ Scripts/Forest.cs
@@ -0,0 +1,32 @@
+﻿using DigitalRuby.RainMaker;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Forest : MonoBehaviour
+{
+    public bool raining;
+    public TMP_Text forecastText;
+    public RainScript rainEffect;
+    public static List<Animator> treeAnimators;
+
+    public void ToggleRain()
+    {
+        // Toggle the rain
+        raining = !raining;
+
+        // Update the forecast text
+        forecastText.text = raining ? "Forecast:\nRaining" : "Forecast:\nCalm";
+
+        // Enable or disable the rain effect
+        rainEffect.RainIntensity = raining ? 1f : 0f;
+
+        // For each tree animator in the forest
+        foreach(Animator treeAnimator in treeAnimators)
+        {
+            // Enalbe or disable the animator depending on the weather
+            treeAnimator.enabled = raining;
+        }
+    }
+}
---
+++ Scripts/TreePlanter.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class TreePlanter : MonoBehaviour
+{
+    public Transform placeholderTree;
+    public Mesh[] treeMeshes;
+    public Forest forest;
+    public List<Animator> treeAnimators = new List<Animator>();
+
+    private void Start()
+    {
+        // Randomize current tree
+        RandomizeTree();
+    }
+
+    public void RandomizeTree()
+    {
+        // Grab a random tree mesh from the list of messhes
+        placeholderTree.GetComponent<MeshFilter>().mesh = treeMeshes[Random.Range(0, treeMeshes.Length)];
+    }
+
+    public void PlantTree()
+    {
+        // Make a clone of the placeholder tree
+        Transform newTree = Instantiate(placeholderTree, placeholderTree.position, placeholderTree.rotation);
+        newTree.SetParent(null, true);
+
+        // Add the new tree's animator to the forest's tree animator list
+        Animator newTreeAnimator = newTree.GetComponent<Animator>();
+        Forest.treeAnimators.Add(newTreeAnimator);
+        newTreeAnimator.enabled = forest.raining;
+
+        // Randomize current tree
+        RandomizeTree();
+    }
+}
